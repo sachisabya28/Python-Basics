07c79622fb054660307c30d9282e5acb818ea541
sachisabya28 <sachisabya28@gmail.com>
2020-05-10 13:48:15 +0530

    dict part2

--- Dictionary/dictionaries.py
+++ Dictionary/dictionaries.py
@@ -4,23 +4,28 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
          "lime": "a sour, green citrus fruit"}
 
-# print(fruit)
-# print(fruit["lemon"])
-# fruit["pear"] = "an odd shaped apple"
-# print(fruit)
-# fruit["lime"] = "great with tequila"
-# print(fruit)
-# # del fruit["lemon"]
-# # fruit.clear()
-# # print(fruit)
-# print(fruit["tomato"])
 print(fruit)
-while True:
-    dict_key = input("Please enter a fruit: ")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit.get(dict_key)
-        print(description)
-    else:
-        print("we don't have a " + dict_key)
+
+# ordered_keys = list(fruit.keys())
+# ordered_keys.sort()
+
+# ordered_keys = sorted(list(fruit.keys()))
+# for f in ordered_keys:
+#     print(f + " - " + fruit[f])
+
+# for f in sorted(fruit.keys()):
+# for f in fruit:
+#     print(f  + " - " + fruit[f])
+# for val in fruit.values():
+#     print(val)
+#
+# print('-' * 40)
+#
+# for key in fruit:
+#     print(fruit[key])
+
+fruit_keys = fruit.keys()
+print(fruit_keys)
+
+fruit["tomato"] = "not nice with ice cream"
+print(fruit_keys)
