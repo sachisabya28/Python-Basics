d19ecfdc140027710ac9573464d55620fe57745b
sachisabya28 <sachisabya28@gmail.com>
2020-05-10 12:13:57 +0530

    All strings operations

--- strings.py
+++ strings.py
@@ -4,8 +4,23 @@ print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
 greeting = "Hello"
-name = input("Please enter your name ")
+name = "Tim"
 print(greeting + name)
 
 # if we want a space, we can add that too
 print(greeting + ' ' + name)
+
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name + f" is {age} years old")
+print(type(age))
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
