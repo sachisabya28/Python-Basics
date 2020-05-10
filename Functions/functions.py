69d90f02a524c35428de050dbec6d6642ae21df6
sachisabya28 <sachisabya28@gmail.com>
2020-05-10 14:05:36 +0530

    func part3

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,10 +1,49 @@
+# A simple Python function to check 
+# whether x is even or odd 
+def evenOdd( x ): 
+    if (x % 2 == 0): 
+        print "even"
+    else: 
+        print "odd"
+  
+# Driver code 
+evenOdd(2) 
+evenOdd(3) 
+
+##############################
+# Here x is a new reference to same list lst 
+def myFun(x): 
+   x[0] = 20
+  
+# Driver Code (Note that lst is modified 
+# after function call. 
+lst = [10, 11, 12, 13, 14, 15]  
+myFun(lst); 
+print(lst)  
+
+
+############################
+def myFun(x): 
+  
+   # After below line link of x with previous 
+   # object gets broken. A new object is assigned 
+   # to x. 
+   x = [20, 30, 40] 
+  
+# Driver Code (Note that lst is not modified 
+# after function call. 
+lst = [10, 11, 12, 13, 14, 15]  
+myFun(lst); 
+print(lst)  
+
+###############################
 def python_food():
     width = 80
     text = "Spam and eggs"
     left_margin = (width - len(text)) // 2
     print(" " * left_margin, text)
 
-
+################################
 def centre_text(*args, sep=' '):
     text = ""
     for arg in args:
@@ -12,6 +51,23 @@ def centre_text(*args, sep=' '):
     left_margin = (80 - len(text)) // 2
     return " " * left_margin + text
 
+##############################
+
+
+
+def swap(x, y): 
+    temp = x; 
+    x = y; 
+    y = temp; 
+  
+# Driver code 
+x = 2
+y = 3
+swap(x, y) 
+print(x) 
+print(y) 
+
+##############################
 # with open("centred", mode='w') as centred_file:
 
 # call the function
